2f550e3fb27f1b0708dcb5198a9c5437624c12bd
Jose Zafrilla Ruiz <55911630+Jozaru27@users.noreply.github.com>
2023-09-28 19:32:36 +0200

    Add files via upload

---
+++ PHP/Ejercicios PHP Básico/ejerciciosphp03.php
@@ -0,0 +1,13 @@
+<?php
+
+/** 
+* @author Jose Zafrilla Ruiz jozaru27@gmail.com
+* 
+* 3. Modifica el ejercicio anterior de modo que el nombre se muestre incluyendo otra línea con
+* “¡Gracias por la visita!"
+*/
+
+$nombre = readline("Por favor, introduzca su nombre. ");
+echo "Hola, " . $nombre . " encantado de conocerte.\n";
+echo "¡Gracias por la visita!\n";
+?>
