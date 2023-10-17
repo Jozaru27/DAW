5f1999fc23b3998b4619d9c299088aa4ad4de43f
Jose Zafrilla Ruiz <55911630+Jozaru27@users.noreply.github.com>
2023-10-17 15:36:06 +0200

    Updated ejercicio14.php
    · Added functioning code
    · Added comments

--- PHP/Ejercicios Estructuras PHP/ejercicio14.php
+++ PHP/Ejercicios Estructuras PHP/ejercicio14.php
@@ -10,6 +10,25 @@
 *
 */
 
+$A = readline("Introduce un número entero para hacer de base \n");
+$B = readline("Introduce un número entero para hacer de exponente \n");
 
+// Vector para las potencias
+$potencias = array();
+
+// Bucle que recorre hasta llegar al número exponente.
+// En una variable, hace como potencia el número base más y el número $i (que se va sumando cada bucle),
+// lo muestra por pantalla y luego lo guarda en el vector antes de volver a empezar.
+for ($i = 1; $i <= $B; $i++) {
+    $res = pow($A, $i);
+    echo "$A^$i = $res \n";
+    $potencias[] = $res; 
+}
+
+// Suma de todas las potencias en el vector
+$suma = array_sum($potencias);
+
+// Muestra la suma por pantalla
+echo "La suma de todas las potencias es: $suma\n";
 
 ?>
