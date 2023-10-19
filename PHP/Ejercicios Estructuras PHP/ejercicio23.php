02b7f40a751a1e279c4daae8f26b7ca9d5d398e6
Jozaru27 <jozaru27@gmail.com>
2023-10-19 16:06:08 +0200

    Uploaded empty activities 23 and 24. Still need to finish 7 and 15

---
+++ PHP/Ejercicios Estructuras PHP/ejercicio23.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+* 
+* @author Jose Zafrilla Ruiz
+*
+* 23. Dado un vector asociativo de trabajadores con su salario creado solicitando al usuario el nombre
+* y salario de cada trabajador, crea usando funciones el salario máximo, el salario mínimo y el
+* salario medio.
+*
+*/
+
+?>
+Pedir por teclado de cuánto quieres que sea el array de trabajadores,
+(nombre readline por salario redline en un for y se sube al array)
+min max y medio salario
---
+++ PHP/Ejercicios Estructuras PHP/ejercicio24.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+* 
+* @author Jose Zafrilla Ruiz
+*
+* 24. Con los trabajadores del ejercicio anterior, calcular el salario actual y el salario aumentado un
+* porcentaje indicado por la variable
+*
+*/
+
+?>
