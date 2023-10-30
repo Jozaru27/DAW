7290bd3f729da68c44836882308596e9c8746cfa
Jozaru27 <jozaru27@gmail.com>
2023-10-30 18:58:59 +0100

    Added Ejercicios Formularios

---
+++ PHP/Ejercicios Formularios/formulario1.php
@@ -0,0 +1,86 @@
+<?php 
+
+/**
+ * 
+ * @author Jose Zafrilla Ruiz
+ * 
+ * Formulario 1
+ * 
+ */
+
+
+if (isset($_GET["ofertas"])){
+    $ofertas = "SÍ";
+} else {
+    $ofertas = "NO";
+}
+
+if (isset($_GET["condiciones"])){
+    $condiciones = "SÍ";
+} else {
+    $condiciones = "NO";
+}
+
+echo "<b>Nombre: </b>" . strtoupper($_GET["nombre"]) . "<br>";
+echo "<b>Apellidos: </b>" . strtoupper($_GET["apellidos"]) . "<br>";
+echo "<b>Sexo: </b>" . strtoupper($_GET["sexo"]) . "<br>";
+echo "<b>Correo: </b>" . strtoupper($_GET["correo"]) . "<br>";
+echo "<b>Provincia: </b>" . strtoupper($_GET["provincia"]) . "<br>";
+echo "<b>Desea recibir información sobre novedades y ofertas: </b> $ofertas" . "<br>";
+echo "<b>Declara haber leído y acepta las condiciones y normativa: </b> $condiciones" . "<br>";
+
+?>
+
+<!-- HTML -->
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8" />
+
+    <!-- Nombre del Autor -->
+    <meta name="author" content="Jose Zafrilla Ruiz" />
+    <link rel="author" href="https://github.com/Jozaru27">
+
+    <!-- Archivo CSS -->
+    <link rel="stylesheet" href="css/style.css" />
+
+    <!-- Título de Página -->
+    <title>Alumnos - Formulario de Registro</title>
+</head>
+
+<body>
+    <h2>Alumnos - Formulario de Registro</h2>
+
+    <form action="formulario1.php" method="get">
+        <fieldset>
+            <legend> Datos Personales </legend><br>
+
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre" maxlength="50"><br><br>
+
+            <label for="apellidos">Apellidos:</label>
+            <input type="text" id="apellidos" name="apellidos" maxlength="50"><br><br>
+
+            <label for="sexo">Sexo:</label><br>
+            <input type="radio" id="sexo" name="sexo" value="H">Hombre
+            <input type="radio" id="sexo" name="sexo" value="M">Mujer<br><br>
+
+            <label for="correo">Correo Electrónico:<label>
+            <input type="text" id="correo" name="correo" maxlength="250"><br> <br>
+
+            <label for="provincia">Provincia:<label>
+            <select name="provincia">
+                <option value="alicante"> Alicante </option>
+                <option value="castellon"> Castellón </option>
+                <option value="valencia"> Valencia </option>
+            </select><br><br>
+
+            <input type="checkbox" name="ofertas" value="" checked> Deseo recibir información sobre novedades y ofertas.<br><br>
+            <input type="checkbox" name="condiciones" value=""> Declaro haber leído y aceptar las condiciones generales del programa y la normativa sobre protección de datos.<br><br>
+
+            <input type="submit" name="enviar" value="Enviar">
+        </fieldset>
+    </form> 
+</body>
+</html>
