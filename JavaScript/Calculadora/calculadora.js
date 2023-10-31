05a05d0c684f184893b71526e9af4d16ab8bea12
Jozaru27 <jozaru27@gmail.com>
2023-10-31 16:41:17 +0100

    Calculadora Javascript Update

--- JavaScript/Calculadora/calculadora.js
+++ JavaScript/Calculadora/calculadora.js
@@ -1,65 +1,98 @@
 /* Cargar un Add Event Listener */
-
-
-let operadores = "+/x-%";
-
-if(operadores.contains("x")) 
-
-
-
 window.onload = function() {
     addEvents();
 }
 
-
+/* EventListener - Cuando se hace click en un cualquier boton, se ejecuta la función acción*/
 function addEvents() {
     let botones = document.querySelectorAll(".boton");
 
     for(boton of botones) {
         boton.addEventListener("click", accion);
-        boton.addEventListener("moused", accion);
-        boton.addEventListener("click", accion);
-        boton.addEventListener("click", accion);
+        boton.addEventListener("mousedown", sombra);
+        boton.addEventListener("mouseup", sombra);
+        boton.addEventListener("mouseout", sombra);
     }
 }
 
+/* Función Acción - La acción que realiza cada botón cuando se pulsa */
 function accion() {
     console.log(this.innerText);
     switch(this.innerText) {
         case "C":
             pantalla.value = "0";
             break;
-        case "X"&&"+":
-            if(comprobarDasduplicidad(asdf)) asdfasdf;
-        case "+":
         case "x":
-
-            laksdfldkjasf
+        case "+":
+        case "-":
+        case "/":
+        case "%":
+            agregarSiDisponible(this.innerText);
+            break;
+        case "«":
+            borrar();
+            break;
+        case "=":
+            resolverExpresion();
+            break;
+        case "()":
+            break;
+        case ".":
+            // solo añadir si hay número delante
+            break;
         default: 
             agregarNumero(this.innerText);
     }
 }
 
-
-
-function comprobarDuplicidad() {
-
-}
-
-
-
-/* Función que togglea si un botón tiene sombra interna o no */
-function sombra(elemento, activar) {
-    if(activar) elemento.classList.add("sombra");
-    else elemento.classList.remove("sombra");
+/* Función Toggle - Sombra interna botón */
+function sombra(ev) {
+    ev.type == "mouseout" ? this.classList.remove("sombra") : this.classList.toggle("sombra");
 }
 
 /* Añadir números al input */
 function agregarNumero(valor) {
-    console.log(valor);
-    //const pantalla = document.querySelector('input');
-    //const numero = boton.id;
-    pantalla.value += valor;
+    if (pantalla.value == "0") pantalla.value = valor;
+    else pantalla.value += valor;
 }
 
-/*botones con onclick y event listener: operandos y operadores - operador que coge la cadena de texto y la calcula*/
+/* Laquo - Borrar */
+function borrar() {
+    let pantalla = document.querySelector('input');
+    let valorPantalla = pantalla.value;
+
+    pantalla.value = valorPantalla.substring(0, valorPantalla.length - 1);
+
+    if (pantalla.value === "") {
+        pantalla.value = "0"; // Si no queda nada, mostrar 0
+    }
+}
+
+/* Función para agregar números si es posible */
+function agregarSiDisponible(valor) {
+    let pantalla = document.querySelector('input');
+    let valorPantalla = pantalla.value;
+    let ultimoCaracter = valorPantalla.charAt(valorPantalla.length - 1);
+
+     // Reemplaza "x" por "*" si se detecta
+    if (valor === "x") {
+        valor = "*";
+    }
+
+    if (!/[X+\/%-,.]/.test(ultimoCaracter)) {
+        agregarNumero(valor);
+    }
+}
+
+/* Función para resolver la expresión y mostrar el resultado en pantalla */
+function resolverExpresion() {
+    let pantalla = document.querySelector('input');
+    let valorPantalla = pantalla.value;
+
+    try {
+        let resultado = eval(valorPantalla);
+        pantalla.value = resultado.toString(); // Muestra el resultado como un número
+    } catch (error) {
+        pantalla.value = "Error"; // Manejar errores en la evaluación
+    }
+}
