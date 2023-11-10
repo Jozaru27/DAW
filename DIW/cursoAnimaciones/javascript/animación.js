6b8f095743978e22d09c38f5c672ed8b69dba455
Jozaru27 <jozaru27@gmail.com>
2023-11-10 17:40:20 +0100

    cursoAnimaciones

--- DIW/cursoAnimaciones/javascript/animación.js
+++ DIW/cursoAnimaciones/javascript/animación.js
@@ -1,36 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Asociar animación de chincheta1 al elemento
-    const chincheta1Element = document.querySelector('.ch1');
-    chincheta1Element.classList.add('mueveChincheta1');
-  
-    // Asociar animación de article1 al elemento
-    const article1Element = document.querySelector('.art1');
-    article1Element.classList.add('animArticle1');
-  
-    // Asociar animación de chincheta2 al elemento después de completar la animación de article1
-    article1Element.addEventListener('animationend', function () {
-      const chincheta2Element = document.querySelector('.ch2');
-      chincheta2Element.classList.add('mueveChincheta2');
-    });
-  
-    // Asociar animación de article2 al elemento después de completar la animación de chincheta2
-    const chincheta2Element = document.querySelector('.ch2');
-    chincheta2Element.addEventListener('animationend', function () {
-      const article2Element = document.querySelector('.art2');
-      article2Element.classList.add('animArticle2');
-    });
-  
-    // Asociar animación de chincheta3 al elemento después de completar la animación de article2
-    article2Element.addEventListener('animationend', function () {
-      const chincheta3Element = document.querySelector('.ch3');
-      chincheta3Element.classList.add('mueveChincheta3');
-    });
-  
-    // Asociar animación de article3 al elemento después de completar la animación de chincheta3
-    const chincheta3Element = document.querySelector('.ch3');
-    chincheta3Element.addEventListener('animationend', function () {
-      const article3Element = document.querySelector('.art3');
-      article3Element.classList.add('animArticle3');
-    });
-  });
-  
+window.onload = function load(){
+
+  let chincheta1Element1 = document.getElementById('chinchi');
+  chincheta1Element1.classList.add('mueveChincheta1');
+
+  const chincheta1Element2 = document.getElementById('chinchi2');
+  chincheta1Element2.classList.add('mueveChincheta2');
+
+  const chincheta1Element3 = document.getElementById('chinchi3');
+  chincheta1Element3.classList.add('mueveChincheta3');
+
+  const articulo1Element = document.getElementsByClassName('art1');
+  articulo1Element.classList.add('mueveChincheta1');
+
+  const articulo2Element = document.getElementsByClassName('art3');
+  articulo2Element.classList.add('mueveChincheta1');
+}
