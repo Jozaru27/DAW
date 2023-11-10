d765aa09b3f7a4e9cd298fe53c7e38677484afc4
Jose Zafrilla Ruiz <jozaru27@gmail.com>
2023-11-10 03:32:03 +0100

    updated hundirLaFlota PHP

--- PHP/Hundir La Flota/hundirLaFlota.php
+++ PHP/Hundir La Flota/hundirLaFlota.php
@@ -14,14 +14,14 @@
  $filas = 8;
  $columnas = 8;
 
- // Inicialización de los barcos y las posiciones.
+ /*// Inicialización de los barcos y las posiciones.
  $b1 = [1,1,1,1];
  $b2 = [1,1,1];
  $b3 = [1,1];
- $b4 = [1];
+ $b4 = [1];*/
 
  // Inicialización de los contadores de barcos e intentos.
- $barcos = 10;
+ $barcosRestantes = 10;
  $intentos = 0;
 
 
@@ -54,43 +54,59 @@ $matriz = array(
     array(1, 0, 0, 0, 1, 0, 0, 0)
 );
 
+// Array asociativo para los barcos, indicando su tamaño, celda inicial y orientación
+$barcos = array(
+    "b1" => array("tamaño" => 4, "fila" => 1, "columna" => 1, "orientacion" => "horizontal"),
+    "b2-1" => array("tamaño" => 3, "fila" => 6, "columna" => 1, "orientacion" => "vertical"),
+    "b2-2" => array("tamaño" => 3, "fila" => 3, "columna" => 4, "orientacion" => "vertical"),
+    "b3-1" => array("tamaño" => 2, "fila" => 0, "columna" => 6, "orientacion" => "horizontal"),
+    "b3-2" => array("tamaño" => 2, "fila" => 2, "columna" => 3, "orientacion" => "vertical"),
+    "b3-3" => array("tamaño" => 2, "fila" => 8, "columna" => 2, "orientacion" => "vertical"),
+    "b4-1" => array("tamaño" => 1, "fila" => 5, "columna" => 5, "orientacion" => "vertical"),
+    "b4-2" => array("tamaño" => 1, "fila" => 6, "columna" => 7, "orientacion" => "vertical"),
+    "b4-3" => array("tamaño" => 1, "fila" => 7, "columna" => 3, "orientacion" => "vertical"),
+    "b4-4" => array("tamaño" => 1, "fila" => 8, "columna" => 5, "orientacion" => "vertical")
+);
+
+
 // Mientras queden barcos, solicitará coordenadas de disparo
-while ($barcos > 0){
+while ($barcoRestantes > 0){
     echo "\nPor favor, introduzca las coordenadas para disparar al tablero [1-8]x[1-8]\n";
     $coordFil = readline("Fila:");
     $coordCol = readline("Columna:");
     $intentos++;
 
-    // Si toca un barco con 1, lo pasa a -1, si es 0 o 2 muestra otra cosa
+    // Si toca un barco (representado con 1), lo pasa a -1
     if ($matriz[$coordFil][$coordCol] == 1){
         echo "Tocado! \n";
         $matriz[$coordFil][$coordCol] = -1;
+        
         // Comprobar si tiene aún celdas con 1 alrededor para confirmar el hundido - Falta por perfilar
         //if ($matriz[$coordFil-1][$coordCol-1] !== 1 && $matriz[$coordFil-1][$coordCol] !== 1 && $matriz[$coordFil-1][$coordCol+1] !== 1 && $matriz[$coordFil][$coordCol-1] !== 1 && $matriz[$coordFil+1][$coordCol-1] !== 1 && $matriz[$coordFil+1][$coordCol+1] !== 1){
         //    echo "Y hundido! \n"; 
         //}
+        
+    // Si toca agua 0, no pasa nada. Igual que si toca un 2
     } else if ($matriz[$coordFil][$coordCol] == 0){
         echo "Agua... \n";
     } else if ($matriz[$coordFil][$coordCol] == 2){
         echo "De nada servirá reducir los restos a cenizas... \n";
     }
 
-    // Mostrar tablero en caso de que haya 1, lo pasa a 0 - Falta por perfilar
-    /*echo "\nTablero:\n";
+    // Mostrar tablero después de cada turno - Los 1 se muestran como uno
+    
+    echo "\nTablero:\n";
     for ($i = 0; $i < $filas; $i++) {
         for ($j = 0; $j < $columnas; $j++) {
-            if ($matriz[$i][$j] == 1){
-                $matriz[$i][$j] = 0;
-            }
-            echo $matriz[$i][$j] . "\t";
+            // Si la celda es 1, mostrar 0 en pantalla sin cambiar el valor en la matriz original
+            echo ($matriz[$i][$j] == 1) ? "0\t" : $matriz[$i][$j] . "\t";
         }
         echo "\n";
-    }*/
-
+    }
 }
 
 // Pantalla de puntuación - Se muestra cuando el número de barcos restantes es 0
-if ($barcos == 0){
+if ($barcosRestantes == 0){
     echo "Fin del juego !! \n";
     echo "Puntuación: \n";
     echo "Total de Intentos ...: \t $intentos \n";
