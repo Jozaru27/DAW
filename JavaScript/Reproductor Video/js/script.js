13733cd119e912ea431cab1bea5d297681017e53
Jozaru27 <jozaru27@gmail.com>
2023-11-28 20:25:36 +0100

    Reproductor Video JS

--- JavaScript/Reproductor Video/js/script.js
+++ JavaScript/Reproductor Video/js/script.js
@@ -1,12 +1,18 @@
 let reproduciendo = false;
+let volumenAlmacenado = 0;
 
 window.onload = () => {
     reanudarPausarButton.addEventListener("click", reanudarPausar);
     silenciarButton.addEventListener("click", silenciar);
+    reiniciarButton.addEventListener("click", reiniciar);
+    subirVolumenButton.addEventListener("click", subirVolumen);
+    bajarVolumenButton.addEventListener("click", bajarVolumen);
+    adelantarVideoButton.addEventListener("click", adelantarVideo);
+    retrocederVideoButton.addEventListener("click", atrasarVideo);
 }
 
-function reanudarPausar(){
-    if (!reproduciendo){
+function reanudarPausar() {
+    if (!reproduciendo) {
         video.play();
         reproduciendo = true;
     } else {
@@ -15,6 +21,34 @@ function reanudarPausar(){
     }
 }
 
-function silenciar(){
-    video.volume = 0;
+function silenciar() {
+    if (video.volume > 0) {
+        volumenAlmacenado = video.volume;
+        video.volume = 0;
+    } else if (video.volume === 0) {
+        video.volume = volumenAlmacenado;
+    }
 }
+
+function reiniciar() {
+    video.load();
+}
+
+function subirVolumen() {
+    video.volume += 0.05;
+    video.volume = parseFloat(video.volume).toFixed(2);
+}
+
+function bajarVolumen() {
+    video.volume -= 0.05;
+    video.volume = parseFloat(video.volume).toFixed(2);
+}
+
+function adelantarVideo() {
+    video.currentTime += 5;
+}
+
+function atrasarVideo(){
+    video.currentTime -= 5;
+}
+
