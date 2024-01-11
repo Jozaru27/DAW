d202db227868b5c691290d0f075e463c59e524bc
Jozaru27 <jozaru27@gmail.com>
2024-01-11 18:19:23 +0100

    POO update

--- PHP/Ejercicios POO/Movil.php
+++ PHP/Ejercicios POO/Movil.php
@@ -5,58 +5,42 @@
 */
 
 class Terminal{
-    private $numero;
+    public $terminal;
+    public $segundosDeLlamada;
 
-    public function __construct($numero){
-        $this->numero = $numero;
-    }
-
-    public function getNumero(){
-        return $this->numero;
-    }
-
-    public function __toString(){
-        return "Nº $this->numero";
-    }
-
-    public function llama(Terminal $t, $segundos){
-        echo "Llamada desde $this->numero a {$t->getNumero()} durante $segundos segundos.\n";
-    }
+    // metodo llama (segundos y tarifa)
 }
 
 class Movil extends Terminal{
-    private $tarifa;
-    private $totalConversacion = 0; // En segundos
+    public $tarifa;
+    public $minutos;
+    public $segundos;
 
-    public function __construct($numero, $tarifa){
-        parent::__construct($numero);
+    public function __construct($terminal, $tarifa){
+        $this->terminal = $terminal;
         $this->tarifa = $tarifa;
     }
 
-    public function __toString(){
-        $minutos = floor($this->totalConversacion / 60);
-        $segundos = $this->totalConversacion % 60;
-        $importe = $this->calcularImporte();
+    public function llama($terminal, $segundosDeLlamada){
+        $this->terminal = $terminal;
+        $this->segundosDeLlamada = $segundosDeLlamada;
 
-        return parent::__toString() . " - $minutos m y $segundos s de conversación en total - tarificados $minutos m y $segundos s por un importe de $importe euros";
-    }
-
-    public function llama(Terminal $t, $segundos){
-        parent::llama($t, $segundos);
-        $this->totalConversacion += $segundos;
-    }
-
-    private function calcularImporte(){
         switch ($this->tarifa) {
             case 'rata':
-                return $this->totalConversacion * 0.06;
+                return $this->segundosDeLlamada * 0.06;
             case 'mono':
-                return $this->totalConversacion * 0.12;
+                return $this->segundosDeLlamada * 0.12;
             case 'bisonte':
-                return $this->totalConversacion * 0.30;
+                return $this->segundosDeLlamada * 0.30;
             default:
                 return 0;
         }
+
+        // if ($segundosDeLlamada > )
+}
+
+    public function __toString(){
+        return "Nº $this->terminal  - 0m y 0s de conversación en total - tarificados 0m y 0 s por un importe de 0 euros<br> \n";
     }
 }
 
---
+++ PHP/Ejercicios POO/awawewe.php
@@ -0,0 +1,57 @@
+class Terminal{
+    private $numero;
+
+    public function __construct($numero){
+        $this->numero = $numero;
+    }
+
+    public function getNumero(){
+        return $this->numero;
+    }
+
+    public function __toString(){
+        return "Nº $this->numero";
+    }
+
+    public function llama(Terminal $t, $segundos){
+        echo "Llamada desde $this->numero a {$t->getNumero()} durante $segundos segundos.\n";
+    }
+}
+
+class Movil extends Terminal{
+    private $tarifa;
+    private $totalConversacion = 0; // En segundos
+
+    public function __construct($numero, $tarifa){
+        parent::__construct($numero);
+        $this->tarifa = $tarifa;
+    }
+
+    public function __toString(){
+        $minutos = floor($this->totalConversacion / 60);
+        $segundos = $this->totalConversacion % 60;
+        $importe = $this->calcularImporte();
+
+        return parent::__toString() . " - $minutos m y $segundos s de conversación en total - tarificados $minutos m y $segundos s por un importe de $importe euros";
+    }
+
+    public function llama(Terminal $t, $segundos){
+        parent::llama($t, $segundos);
+        $this->totalConversacion += $segundos;
+    }
+
+    private function calcularImporte(){
+        switch ($this->tarifa) {
+            case 'rata':
+                return $this->totalConversacion * 0.06;
+            case 'mono':
+                return $this->totalConversacion * 0.12;
+            case 'bisonte':
+                return $this->totalConversacion * 0.30;
+            default:
+                return 0;
+        }
+    }
+}
+
+
