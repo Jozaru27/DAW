d3381c91a8d5d30e204995991135a10bcccb6bd2
Jozaru27 <jozaru27@gmail.com>
2024-02-08 15:44:15 +0100

    cosituquis

--- JavaScript/JSProvincias/js/main.js
+++ JavaScript/JSProvincias/js/main.js
@@ -12,7 +12,6 @@ function cargarDatos() {
     fetch(URLDatos)
         .then((datos) => datos.json())
         .then((dato) => {
-            console.log(dato);
             datos = dato.results;
             cargarProvincias();
         });
@@ -21,7 +20,7 @@ function cargarDatos() {
 // Función la cuál selecciona todos los elementos con la clase .comunidad y lo guarda en la variable lista
 // Luego, con un for recorre cada uno de los elementos, y a los anchor les mete un eventListener para cuando se haga click que llame a cargar Evento
 function cargarProvincias() {
-    let lista = document.querySelectorAll(".comunidad"); 
+    let lista = document.querySelectorAll(".comunidad");
 
     for (let comunidad of lista) {
         if (comunidad.querySelector("a")) {
@@ -43,12 +42,29 @@ function cargarEvento() {
     tableRow.innerText = comunidad;
 
     for (let provincias of datos) {
-        console.log(provincias.ccaa);
+     //   console.log(provincias.ccaa);
         if (provincias.ccaa == comunidad) {
             let col = document.createElement("td");
 
             col.appendChild(document.createTextNode(provincias.provincia));
             table.appendChild(col);
+
+            console.log(provincias);
+
+            col.dataset.lat = provincias.geo_point_2d.lat;
+            col.dataset.lon = provincias.geo_point_2d.lon;
+
+            col.addEventListener("click", mapituqui);
         }
     }
+
+
+}
+
+function mapituqui() {
+
+    let urlMapa = "https://www.openstreetmap.org/#map=14/" + this.dataset.lat + "/" + this.dataset.lon;
+
+    window.open(urlMapa);
+
 }
